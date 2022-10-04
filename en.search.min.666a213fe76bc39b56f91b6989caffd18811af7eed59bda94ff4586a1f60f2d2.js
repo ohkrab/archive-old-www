68550f1f09bf014c7fa6b01fde305d7f13135797
qbart <qbart@users.noreply.github.com>
2022-10-04 21:37:10 +0000

    deploy: 260838e9acdf2f7325bb7b4f1f366599ae9722cb

--- en.search.min.666a213fe76bc39b56f91b6989caffd18811af7eed59bda94ff4586a1f60f2d2.js
+++ en.search.min.666a213fe76bc39b56f91b6989caffd18811af7eed59bda94ff4586a1f60f2d2.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.4d88a413d80f57ab87bb5015173c6d9e51cc7b14ca7b0e3ed565c550df7bc39b.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.664f74e12d45b9f759fbbfec3e478e294427100eccb83fe21f7a5b193ee579e9.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
